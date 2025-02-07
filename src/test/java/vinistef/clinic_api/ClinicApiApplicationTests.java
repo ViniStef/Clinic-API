8285b46e24836fb61de28a59ee11ae65cba9b93e
Vinicius <145880205+ViniStef@users.noreply.github.com>
2025-02-07 13:37:12 -0300

    Initial spring boot api setup

---
+++ src/test/java/vinistef/clinic_api/ClinicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vinistef.clinic_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
