c18a4123ffd03865955edfba0fec3e0869eb5f52
Vinicius <145880205+ViniStef@users.noreply.github.com>
2025-02-14 13:30:04 -0300

    Added validation to patient register fields

--- src/main/java/vinistef/clinic_api/dto/RegisterPatientDto.java
+++ src/main/java/vinistef/clinic_api/dto/RegisterPatientDto.java
@@ -1,4 +1,26 @@
 package vinistef.clinic_api.dto;
 
-public record RegisterPatientDto(String name, String email, String cellphone, String cpf, AddressDataDto addressDto) {
+import jakarta.persistence.Embedded;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record RegisterPatientDto(
+        @NotBlank
+        String name,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{9,11}")
+        String cellphone,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{11}")
+        String cpf,
+
+        @Embedded
+        AddressDataDto addressDto) {
 }
--- src/main/java/vinistef/clinic_api/entity/Patient.java
+++ src/main/java/vinistef/clinic_api/entity/Patient.java
@@ -1,6 +1,9 @@
 package vinistef.clinic_api.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
