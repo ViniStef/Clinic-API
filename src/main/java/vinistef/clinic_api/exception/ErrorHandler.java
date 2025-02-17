0d79ea3704ab948d8251abe75c748bbe1a5da03c
Vinicius <145880205+ViniStef@users.noreply.github.com>
2025-02-17 15:41:59 -0300

    Added extra error handling

--- src/main/java/vinistef/clinic_api/exception/ErrorHandler.java
+++ src/main/java/vinistef/clinic_api/exception/ErrorHandler.java
@@ -1,7 +1,10 @@
 package vinistef.clinic_api.exception;
 
-import org.springframework.http.RequestEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,6 +26,26 @@ public class ErrorHandler {
         var errors = exception.getFieldErrors();
 
         return ResponseEntity.badRequest().body(errors.stream().map(ErrorResponseDto::new).toList());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleError400(HttpMessageNotReadableException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<String> handleErrorAuthentication() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleErrorAccessDenied() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleError500(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
     }
 
     private record ErrorResponseDto(String name, String message) {
